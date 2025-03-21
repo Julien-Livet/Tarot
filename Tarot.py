ec690d00eddf7cd7f4381d99e4d4b626882b5b99
Julien <julien.livet@free.fr>
2025-03-21 19:35:40 +0100

    Update

--- Tarot.py
+++ Tarot.py
@@ -1242,10 +1242,10 @@ class Player:
                         if (list(emptyFamilies.values()).count(True) == 4):
                             index = 0
                                 
-                            if (assets[index].name() == "asset-1"):
+                            if (handAssets[index].name() == "asset-1"):
                                 index -= 1
                             
-                            selectedCard = choices.index(assets[index])
+                            selectedCard = choices.index(handAssets[index])
                         else:
                             i = 0
                                         
@@ -1340,6 +1340,7 @@ class Player:
                         
                         try:
                             selectedCard = choices[index]
+                            selectedCard = index
                         except:
                             pass
 
